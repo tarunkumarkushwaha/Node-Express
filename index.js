e8c649a68384f493795df758da203dd5b11a385f
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-03-13 18:05:24 +0530

    netlideploy settings update 1

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express')
+const serverless = require('serverless-http')
 const path = require('path')
+const { Router } = require('express')
 const app = express()
-const port = 3000
+// const port = 3000
+const router = express.Router()
 
-app.use(express.static(path.join(__dirname,"static")))
-
-app.get('/', (req, res) => {
-//   res.send('Hello World!')
-  res.sendFile(path.join(__dirname,'package.json'))
+router.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,'response.json'))
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port on ${port}`)
-})
+// app.listen(port, () => {
+//   console.log(`Example app listening on port on ${port}`)
+// })
+app.use('/.netlify/functions/api',router)
+module.exports.handlers(app)
