848b20e908d2c6860b525b38514c23ff3f472df5
Tarun Kushwaha <116190775+tarunkumarkushwaha@users.noreply.github.com>
2023-03-13 18:23:15 +0530

    Revert "netlideploy settings update 1"
    This reverts commit e8c649a68384f493795df758da203dd5b11a385f.

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const express = require('express')
-const serverless = require('serverless-http')
 const path = require('path')
-const { Router } = require('express')
 const app = express()
-// const port = 3000
-const router = express.Router()
+const port = 3000
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'response.json'))
+app.use(express.static(path.join(__dirname,"static")))
+
+app.get('/', (req, res) => {
+//   res.send('Hello World!')
+  res.sendFile(path.join(__dirname,'package.json'))
 })
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port on ${port}`)
-// })
-app.use('/.netlify/functions/api',router)
-module.exports.handlers(app)
+app.listen(port, () => {
+  console.log(`Example app listening on port on ${port}`)
+})
